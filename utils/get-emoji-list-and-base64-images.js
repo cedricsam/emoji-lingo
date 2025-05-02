28fbac02b0a0c3f4373b859d8088893c49ceb4ef
Cedric Sam <cedricsam@gmail.com>
2025-05-02 00:23:25 -0400

    extract base64 for google from github-hosted images

--- utils/get-emoji-list-and-base64-images.js
+++ utils/get-emoji-list-and-base64-images.js
@@ -59,42 +59,56 @@ const promises = Array.from(rows).map((row) => {
   const no = $(row).find('td.rchars').text();
   const name = $(row).find('td.name').text();
   const code = $(row).find('td.code').text();
-  const codeTrimmed = code
-    .replaceAll(/U\+200D\b/g, '')
+  const codeTrimmed = code // Keep the joiners, as they will be present in emoji codes used for joining with the CLDR data
     .replaceAll(/U\+FE0F\b/g, '')
     .replace(/U\+/g, '')
     .toLowerCase()
     .trim();
-  const codeArray = codeTrimmed.split(/\s+/);
+  const codeArrayAll = codeTrimmed.split(/\s+/);
   if (Number.isFinite(+no) && +no) {
-    const hasGenderCodeIndex = codeArray.findIndex((code) => genderCodes[code]);
-    let genderCode;
-    if (hasGenderCodeIndex !== -1 && codeArray.length > 1) {
-      genderCode = genderCodes[codeArray[hasGenderCodeIndex]];
-      codeArray.splice(hasGenderCodeIndex, 1);
-    }
-    // if more than one code, join the uppercased codes with underscores
-    const codeGlyphExtractor = `${
-      codeArray[0].replace(/^0+/, '')
-    }${
-      codeArray.length > 1 && `_${codeArray.slice(1).map((c) => `u${c.toUpperCase()}`).join('_')}` || ''
-    }`;
-    const genderCodeStr = genderCode && `.${genderCode}` || '';
-    const imgPath = `${EMOJIS_PNG_INDIR}/0x${codeGlyphExtractor}${genderCodeStr}.png`; // matches the glyph_extractor file names (on Apple emoji glyphs)
-    try {
-      const imgData = readFileSync(imgPath);
-      if (verbose >= 3) console.log(`✅`, +no, code, codeTrimmed, codeArray, imgPath, name);
-      return sharpenImage(imgData, +no, codeTrimmed);
-    } catch {
+    if (vendor === `google`) { // For Google, using https://github.com/googlefonts/noto-emoji
+      // File name starts with "emoji_u", then the "_" underscore separated codes, including joiners
+      const imgPath = `${EMOJIS_PNG_INDIR}/emoji_u${codeArrayAll.join("_")}.png`; // matches the glyph_extractor file names (on Apple emoji glyphs)
       try {
-        const skinToneModifier = codeArray.length > 2 && codeArray[1] === `1f91d` ? `66` : `0`; // 66 is for the very special case of two people holding hands, where the modifier is 66
-        const skinToneNeutralImgPath = `${EMOJIS_PNG_INDIR}/0x${codeGlyphExtractor}.${skinToneModifier}${genderCodeStr}.png`; // matches the glyph_extractor file names (on Apple emoji glyphs)
-        const skinToneNeutralImgData = readFileSync(skinToneNeutralImgPath);
-        if (verbose >= 2) console.log(`⚠️ skin tone neutral modifier needed`, +no, code, codeTrimmed, codeArray, skinToneNeutralImgPath, name);
-        return sharpenImage(skinToneNeutralImgData, +no, codeTrimmed);
+        const imgData = readFileSync(imgPath);
+        if (verbose >= 3) console.log(`✅`, +no, code, codeTrimmed, codeArrayAll, imgPath, name);
+        return sharpenImage(imgData, +no, codeTrimmed);
       } catch {
-        if (verbose >= 1) console.log(`❌`, +no, code, codeTrimmed, codeArray, codeGlyphExtractor, name);
+        if (verbose >= 1) console.log(`❌`, +no, code, codeTrimmed, codeArrayAll, name);
         return undefined;
+      }
+    } else if (vendor === `apple`) { // For Apple, using Glyph Extractor (https://github.com/faveris/glyph_extractor) to get images from font...
+      const codeArray = codeArrayAll.filter((d) => d !== '200d');
+      // File name starts with 0x, excludes "200d" joiners, appends a number for skin tone modifier and a letter for gender
+      const hasGenderCodeIndex = codeArray.findIndex((code) => genderCodes[code]);
+      let genderCode;
+      if (hasGenderCodeIndex !== -1 && codeArray.length > 1) {
+        genderCode = genderCodes[codeArray[hasGenderCodeIndex]];
+        codeArray.splice(hasGenderCodeIndex, 1);
+      }
+      // if more than one code, join the uppercased codes with underscores
+      const codeGlyphExtractor = `${
+        codeArray[0].replace(/^0+/, '')
+      }${
+        codeArray.length > 1 && `_${codeArray.slice(1).map((c) => `u${c.toUpperCase()}`).join('_')}` || ''
+      }`;
+      const genderCodeStr = genderCode && `.${genderCode}` || '';
+      const imgPath = `${EMOJIS_PNG_INDIR}/0x${codeGlyphExtractor}${genderCodeStr}.png`; // matches the glyph_extractor file names (on Apple emoji glyphs)
+      try {
+        const imgData = readFileSync(imgPath);
+        if (verbose >= 3) console.log(`✅`, +no, code, codeTrimmed, codeArray, imgPath, name);
+        return sharpenImage(imgData, +no, codeTrimmed);
+      } catch {
+        try {
+          const skinToneModifier = codeArray[1] === `1f91d` ? `66` : `0`; // 66 is for the very special case of two people holding hands, where the modifier is 66
+          const skinToneNeutralImgPath = `${EMOJIS_PNG_INDIR}/0x${codeGlyphExtractor}.${skinToneModifier}${genderCodeStr}.png`; // matches the glyph_extractor file names (on Apple emoji glyphs)
+          const skinToneNeutralImgData = readFileSync(skinToneNeutralImgPath);
+          if (verbose >= 2) console.log(`⚠️ skin tone neutral modifier needed`, +no, code, codeTrimmed, codeArray, skinToneNeutralImgPath, name);
+          return sharpenImage(skinToneNeutralImgData, +no, codeTrimmed);
+        } catch {
+          if (verbose >= 1) console.log(`❌`, +no, code, codeTrimmed, codeArray, codeGlyphExtractor, name);
+          return undefined;
+        }
       }
     }
   }
